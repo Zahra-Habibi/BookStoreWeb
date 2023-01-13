6e37039bbcc03b529c3d34deb19978b2c42bc9ec
ZahraHabibi <habibi.zahra.ku@gmail.com>
2023-01-13 08:40:24 -0800

    bulkybook site created

---
+++ BulkyBook/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BulkyBook.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBook.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> categories { get; set; }
+    }
+}
---
+++ BulkyBook/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBook.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreateDateTime { get; set; }=DateTime.Now;
+
+
+
+    }
+}
