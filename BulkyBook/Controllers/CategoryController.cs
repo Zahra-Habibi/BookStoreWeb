6c465097804421491b66d3ac768f4a6050e11f2a
ZahraHabibi <habibi.zahra.ku@gmail.com>
2023-02-12 06:31:33 -0800

    servir site validation in categorycontroller done

--- BulkyBook/Controllers/CategoryController.cs
+++ BulkyBook/Controllers/CategoryController.cs
@@ -28,9 +28,14 @@ namespace BulkyBook.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
         {
-            _db.categories.Add(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                _db.categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+           
         }
     }
 }
