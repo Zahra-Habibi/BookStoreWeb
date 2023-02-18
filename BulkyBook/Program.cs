4fb2820e7b24bda81e6ca852f9c75da31eb84d08
ZahraHabibi <habibi.zahra.ku@gmail.com>
2023-02-18 07:54:12 -0800

    interfaces added in the program file

--- BulkyBook/Program.cs
+++ BulkyBook/Program.cs
@@ -1,4 +1,5 @@
 using BulkyBook.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +11,8 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
+builder.Services.AddScoped<ICategoryRepository, ICategoryRepository>();
+builder.Services.AddMvc();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
