99fbdf759095aaa7eb53ccef14757a7b5bd55c23
ZahraHabibi <habibi.zahra.ku@gmail.com>
2023-02-18 06:52:54 -0800

    repository method done

---
+++ BulkyBook.DataAccess/Repository/Repository.cs
@@ -0,0 +1,51 @@
+﻿using BulkyBook.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> DbSet;
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            this.DbSet = db.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            DbSet.Add(entity);
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null)
+        {
+            IQueryable<T> query = DbSet;
+            return query.ToList();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter, string? includeProperties = null, bool tracked = true)
+        {
+            IQueryable<T> query = DbSet;
+            query= query.Where(filter);
+            return query.FirstOrDefault();
+        }
+
+        public void Remove(T entity)
+        {
+            DbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            DbSet.RemoveRange(entity);
+        }
+    }
+}
