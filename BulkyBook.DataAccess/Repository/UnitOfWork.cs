6e517138137f603aa3f538ac464362b25d7cd816
ZahraHabibi <habibi.zahra.ku@gmail.com>
2023-02-18 22:11:35 -0800

    unitofwork repository created

---
+++ BulkyBook.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using BulkyBook.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Category = new CategoryRepository(_db);
+        }
+
+        public ICategoryRepository Category { get; private set; }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
