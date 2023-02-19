d75bbe89aa3a2429f4a484adbe7d084a198fd1b3
ZahraHabibi <habibi.zahra.ku@gmail.com>
2023-02-19 08:43:14 -0800

    covertype repository completed

---
+++ BulkyBook.DataAccess/Repository/CoverTypeRepository.cs
@@ -0,0 +1,26 @@
+﻿using BulkyBook.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class CoverTypeRepository : Repository<CoverType>, ICoverTypeRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CoverTypeRepository(ApplicationDbContext db) :base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(CoverType obj)
+        {
+            _db.coverTypes.Update(obj);
+        }
+    }
+}
