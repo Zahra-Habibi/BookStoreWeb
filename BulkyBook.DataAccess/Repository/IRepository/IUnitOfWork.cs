1968875e935a5714368589bce1f59dc89f5442fe
ZahraHabibi <habibi.zahra.ku@gmail.com>
2023-02-19 08:43:53 -0800

    unitofwork for covertype completed

--- BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -9,6 +9,7 @@ namespace BulkyBook.DataAccess.Repository.IRepository
     public interface IUnitOfWork
     {
         ICategoryRepository Category { get; }
+        ICoverTypeRepository CoverType { get; }
         void Save();
     }
 }
