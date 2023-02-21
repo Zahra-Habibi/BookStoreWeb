5fef1c9515c0bf5b85324722860ad843932df457
ZahraHabibi <habibi.zahra.ku@gmail.com>
2023-02-21 08:55:43 -0800

    some changes added in the project

--- BulkyBook.Models/Models/CoverType.cs
+++ BulkyBook.Models/Models/CoverType.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace BulkyBook.Models.Models
 {
@@ -15,6 +10,8 @@ namespace BulkyBook.Models.Models
         [Display(Name="Cover Type")]
         [Required]
         [MaxLength(50)]
+
+
         public string Name { get; set; }
     }
 }
