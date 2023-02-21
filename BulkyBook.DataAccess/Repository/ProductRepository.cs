41fd6be94a29e6019cd6511c6e41aa375f337ee3
ZahraHabibi <habibi.zahra.ku@gmail.com>
2023-02-21 09:22:04 -0800

    product repository added

---
+++ BulkyBook.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,41 @@
+﻿using BulkyBook.Data;
+using BulkyBook.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class ProductRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void Update(Product obj)
+        {
+            var objFromDb = _db.products.FirstOrDefault(u => u.Id == obj.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Title = obj.Title;
+                objFromDb.ISBN = obj.ISBN;
+                objFromDb.Price = obj.Price;
+                objFromDb.Price50 = obj.Price50;
+                objFromDb.ListPrice = obj.ListPrice;
+                objFromDb.Price100 = obj.Price100;
+                objFromDb.Description = obj.Description;
+                objFromDb.CategoryId = obj.CategoryId;
+                objFromDb.Author = obj.Author;
+                objFromDb.CoverTypId = obj.CoverTypId;
+                if (obj.ImgUrl != null)
+                {
+                    objFromDb.ImgUrl = obj.ImgUrl;
+                }
+            }
+        }
+    }
+}
