a61a2c86c1268f171edbe8c286f3e23cc6338f2c
ZahraHabibi <habibi.zahra.ku@gmail.com>
2023-08-12 06:21:46 -0700

    model created

---
+++ BulkyBookRaza/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookRaza.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreateDateTime { get; set; } = DateTime.Now;
+
+    }
+}
