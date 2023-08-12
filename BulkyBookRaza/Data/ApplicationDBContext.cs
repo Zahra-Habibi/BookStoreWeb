42f77b40a693534dba1261f9615e1a4c2d44728d
ZahraHabibi <habibi.zahra.ku@gmail.com>
2023-08-12 06:51:28 -0700

    database connection added

---
+++ BulkyBookRaza/Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using BulkyBookRaza.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookRaza.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> categories { get; set; }
+    
+    }
+}
