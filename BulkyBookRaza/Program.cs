0c7f70910309203bdb75381153a0235ca1071a05
ZahraHabibi <habibi.zahra.ku@gmail.com>
2023-08-12 06:50:59 -0700

    programfile managed

--- BulkyBookRaza/Program.cs
+++ BulkyBookRaza/Program.cs
@@ -1,7 +1,15 @@
+using BulkyBookRaza.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+//Connection with database
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 
 var app = builder.Build();
 
