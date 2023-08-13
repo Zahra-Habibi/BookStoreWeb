9b837081a28325a2e5bdbff9979e9350773113e9
ZahraHabibi <habibi.zahra.ku@gmail.com>
2023-08-13 08:41:49 -0700

    first project of razorpage created for category

---
+++ BulkyBookRaza/Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyBookRaza.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
