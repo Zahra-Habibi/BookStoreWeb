6156a442bc37a5859956a5be2ee9a902fa979f8f
ZahraHabibi <habibi.zahra.ku@gmail.com>
2023-08-13 09:27:50 -0700

    some changes added in the indexcontroller

--- BulkyBookRaza/Pages/Categories/Index.cshtml.cs
+++ BulkyBookRaza/Pages/Categories/Index.cshtml.cs
@@ -1,12 +1,23 @@
+using BulkyBookRaza.Data;
+using BulkyBookRaza.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
 
 namespace BulkyBookRaza.Pages.Categories
 {
     public class IndexModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
+        public List<Category> CategoryList { get; set; }
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public void OnGet()
         {
+            CategoryList = _db.categories.ToList();
         }
     }
 }
