4966e27246a3f9c23cabcffea396849af6208fe6
ZahraHabibi <habibi.zahra.ku@gmail.com>
2023-08-15 07:48:37 -0700

    delelte page completed

--- BulkyBookRaza/Pages/Categories/Delete.cshtml.cs
+++ BulkyBookRaza/Pages/Categories/Delete.cshtml.cs
@@ -1,12 +1,39 @@
+using BulkyBookRaza.Data;
+using BulkyBookRaza.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BulkyBookRaza.Pages.Categories
 {
+    [BindProperties]
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDBContext _db;
+        [BindProperty]
+        public Category? Category { get; set; }
+        public DeleteModel(ApplicationDBContext db)
         {
+            _db = db;
+        }
+        public void OnGet(int? id)
+        {
+            if (id != null && id != 0)
+            {
+                Category = _db.categories.Find(id);
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            Category obj = _db.categories.Find(Category.Id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToPage("Index");
         }
     }
 }
